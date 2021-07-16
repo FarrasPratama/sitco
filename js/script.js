e4d3659178b0e4329394a4cfce81a9898d944ae7
FarrasPratama <64601750+FarrasPratama@users.noreply.github.com>
2021-07-16 16:52:47 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-const parentContainer = document.querySelector('.container');
-parentContainer.addEventListener('click', event=>{
-    const current = event.target;
-    const isRadMoreBtn = current.className.includes('main_bt');
-    if(!isRadMoreBtn) return;
-    const currentText = event.target.parentNode.querySelector('.read-more-text');
-    currentText.classList.toggle('read-more-text--show');
-    current.textContent = current.textContent.includes('Read More') ? "Read Less" : "Read More";
-})
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
