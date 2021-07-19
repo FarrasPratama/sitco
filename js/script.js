484ca1ca248f48799e9d15a5be97aa841d3e3d0a
FarrasPratama <64601750+FarrasPratama@users.noreply.github.com>
2021-07-19 12:36:33 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,3 +13,33 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("dots2").style.display = "none";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("dots2").style.display = "inline";
+  }
+
+  function on1() {
+    document.getElementById("overlay1").style.display = "block";
+    document.getElementById("dots21").style.display = "none";
+  }
+  
+  function off1() {
+    document.getElementById("overlay1").style.display = "none";
+    document.getElementById("dots21").style.display = "inline";
+  }
+
+  function on2() {
+    document.getElementById("overlay2").style.display = "block";
+    document.getElementById("dots22").style.display = "none";
+  }
+  
+  function off2() {
+    document.getElementById("overlay2").style.display = "none";
+    document.getElementById("dots22").style.display = "inline";
+  }
